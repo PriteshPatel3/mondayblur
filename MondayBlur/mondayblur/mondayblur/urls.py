3672c95983847507d6e477bf4708dfdebd7159e4
priteshpateldev <priteshpateldev@gmail.com>
2019-04-25 09:58:29 +0800

    fix something with registration

--- MondayBlur/mondayblur/mondayblur/urls.py
+++ MondayBlur/mondayblur/mondayblur/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from User import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('QnA.urls')),
+    path('register/', views.register, name='register')
     
 ]
