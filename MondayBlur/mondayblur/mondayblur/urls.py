17c3c96304ff2f3ef10eb4148097363d8cd8522a
priteshpateldev <priteshpateldev@gmail.com>
2019-05-05 00:09:44 +0800

    profile pic working now

--- MondayBlur/mondayblur/mondayblur/urls.py
+++ MondayBlur/mondayblur/mondayblur/urls.py
@@ -18,7 +18,9 @@ from django.urls import path,include
 from users import views
 from django.contrib.auth import views as auth_views
 from users.views import ProfileListView
-
+from mondayblur import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +30,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
