542d8f56ea37d2e2d2cf7ad5b1020798279c3b8e
priteshpateldev <priteshpateldev@gmail.com>
2019-05-09 23:39:41 +0800

    added profile update

--- MondayBlur/mondayblur/mondayblur/urls.py
+++ MondayBlur/mondayblur/mondayblur/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from users import views
 from django.contrib.auth import views as auth_views
-from users.views import ProfileListView
+from users.views import ProfileListView, profile
 from mondayblur import settings
 from django.conf import settings
 from django.conf.urls.static import static
@@ -29,5 +29,6 @@ urlpatterns = [
     path('profile/', ProfileListView.as_view(template_name='users/profile.html'), name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('update/', views.profile, name='update'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- MondayBlur/mondayblur/users/views.py
+++ MondayBlur/mondayblur/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm, User
+from .forms import UserRegisterForm, User, UserUpdateForm, ProfileUpdateForm
 from QnA.models import question
 from django.views.generic import ListView
 
@@ -23,7 +23,35 @@ class ProfileListView(ListView):
     template_name : 'users/profile.html'
     context_object_name = 'question'
     ordering = ['-date_published']
-    paginate_by = 5      
+    paginate_by = 5
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
+
+
+
 
 
 
