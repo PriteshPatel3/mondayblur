b8f9393586888b2fab1c01fde1317c7c5bf4a4f6
kelvin6666 <1181101297@student.mmu.edu.my>
2019-05-12 22:19:36 +0800

    database

--- MondayBlur/mondayblur/users/models.py
+++ MondayBlur/mondayblur/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from QnA.models import comment
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,4 +11,21 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
+
+#database for points
+class Reward(models.Model): 
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
+
+    
+
+
+
+
+
+
+
+
+        
+
 
